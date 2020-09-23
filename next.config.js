cfc5648752e5d90d556190016850b7f23e5fef8d
Mucahid Yazar <mucahidyazar@gmail.com>
2020-09-23 13:57:21 +0300

    Added css module system for sass and edited node-sass implementation for this project. It was only node-sass then I added @zeit/next-sass correctly

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
-const { nextI18NextRewrites } = require("next-i18next/rewrites");
+const withPlugins = require('next-compose-plugins')
+const { nextI18NextRewrites } = require('next-i18next/rewrites')
+const withSass = require('@zeit/next-sass')
+const localeSubpaths = {}
 
-const localeSubpaths = {};
-
-module.exports = {
+const nextConfig = {
   rewrites: async () => nextI18NextRewrites(localeSubpaths),
   publicRuntimeConfig: {
     localeSubpaths,
   },
-};
+}
+
+module.exports = withPlugins([[withSass, { cssModules: true }]], nextConfig)
