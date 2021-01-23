0d61dbb21641da63c54cfdc27d5a2e2932373312
Mucahid Yazar <mucahidyazar@gmail.com>
2021-01-23 16:25:15 +0300

    Added Nextjs Internationalized and translate with next-translate, added nprogress to see loading of the pages, added toastify for beautiful notifications, conwerted wrapper to the Provider of redux, added normalize.css, added some eslint settings, added react modal, and added some global settings some of these modules

--- pages/_error/index.tsx
+++ pages/_error/index.tsx
@@ -1,19 +1,19 @@
 import Head from 'next/head'
 import styles from './styles.module.scss'
 
-function NotFound() {
+function Error() {
   return (
     <div className={styles.main}>
       <Head>
-        <title>NotFound Page</title>
+        <title>Error Page</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <div className={styles.notFound}>
+      <div className={styles.error}>
         <img style={{ width: 500 }} src="static/assets/gif/404.gif" alt="" />
       </div>
     </div>
   )
 }
 
-export default NotFound
+export default Error
